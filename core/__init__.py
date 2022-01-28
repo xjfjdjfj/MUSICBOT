30da28ee19c59a0b9e228a21b3c2eae967c7c1dc
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-01-28 20:23:20 +0530

    Add files via upload

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,25 @@
-
+from core.funcs import (
+    app,
+    ydl,
+    search,
+    legend,
+    pytgcalls,
+    extract_args,
+    check_yt_url,
+    skip_stream,
+    start_stream,
+    generate_cover,
+    delete_messages,
+    get_youtube_playlist,
+)
+from core.groups import (
+    all_groups,
+    set_default,
+    get_group,
+    set_group,
+    set_title,
+    get_queue,
+    clear_queue,
+    shuffle_queue,
+)
+from core.song import Song
