cfac8417da143f2648130ca9ad74631ed5ef540b
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-03-06 13:02:29 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/LEGEND-AI/LEGEND-MUSIC /
+git clone https://github.com/LEGEND-AI/LEGEND-MUSIC /LEGEND-MUSIC
 echo ">> INSTALLING REQUIREMENTS..."
 cd /LEGEND-MUSIC
 pip3 install -U -r requirements.txt
@@ -13,15 +13,7 @@ echo "
 #     # #    #      # # #      
 #     # #    # #    # # #    # 
 #     #  ####   ####  #  ####  
-                               
-######                                    
-#     # #        ##   #   # ###### #####  
-#     # #       #  #   # #  #      #    # 
-######  #      #    #   #   #####  #    # 
-#       #      ######   #   #      #####  
-#       #      #    #   #   #      #   #  
-#       ###### #    #   #   ###### #    # 
-                                          
+                                                                          
 MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
 "
 python3 main.py
